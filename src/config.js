809f15c08dcf197ecdb76b78b763d825d33ebe18
AlinaTaoRao <raotao77@outlook.com>
2022-05-31 20:32:10 +0200

    small change

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const localHostUrl="http://localhost:3000/api";
+export const localhostUrl="http://localhost:3000/api";
 export const herokuUrl="https://stemers-backend-heroku.herokuapp.com/api";
 export const baseUrl=herokuUrl;
