7f10cb711f321f8e277ac7533970d567c6581e8e
AlinaTaoRao <raotao77@outlook.com>
2022-06-06 17:10:50 +0200

    add STEMers components

---
+++ src/components/about/About.js
@@ -0,0 +1,12 @@
+import './styles.css';
+
+
+export default function About () {
+
+return(
+    <div className='about'>
+        Template About Page 
+    </div>
+)
+
+}
---
+++ src/components/sign-in/SignInOrSignUp.js
@@ -0,0 +1,13 @@
+import './styles.css';
+
+
+export default function SignInOrSignUp () {
+
+
+return(
+    <div className='sign-in-or-sign-up'>
+        Template sign in or sign up page
+    </div>
+)
+
+}
