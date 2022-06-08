0aa5968e6c8f2019b70ef26514ec6ce1324abb8a
AlinaTaoRao <raotao77@outlook.com>
2022-06-08 21:00:52 +0200

    remove comment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
-// import logo from './logo.png';
 import "./App.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
@@ -31,20 +30,3 @@ function App() {
 }
 
 export default App;
-
-/* 
-  <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-*/
