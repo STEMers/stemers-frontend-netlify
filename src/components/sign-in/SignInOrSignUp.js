578338e9206dab051d537d2744bbaadacc1b3a8b
AlinaTaoRao <raotao77@outlook.com>
2022-06-09 10:14:59 +0200

    signin draft and  kit's code into index.html

--- src/components/sign-in/SignInOrSignUp.js
+++ src/components/sign-in/SignInOrSignUp.js
@@ -1,13 +1,43 @@
-import './styles.css';
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
+import "./styles.css";
 
-export default function SignInOrSignUp () {
+export default function SignInOrSignUp() {
+  const initialValues = { username: "", email: "", password: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const navigate = useNavigate();
 
+  return (
+    <div className="sign-in-or-sign-up">
+      <form name="sign-in--form" className="sign-in-form">
+      <label htmlFor="usr-email">Username/Email:</label>
+        <input
+          type="text"
+          name="usr-email"
+          id="usr-email"
+          placeholder="Enter username or email"
+          className="sign-in--uer-email"
+          value={formValues.email}
+          required
+        />
+        <p className="error-username error"> email error</p>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Enter Password"
+          className="sign-in--password"
+          value={formValues.password}
+          required
+        />
+        <p className="error-password error"> password error</p>
+        <input type="checkbox" name="sign-in--checkbox" id="remember-me"/>
+        <label htmlFor="remember-me">Remember me</label>
+        
 
-return(
-    <div className='sign-in-or-sign-up'>
-        Template sign in or sign up page
+      </form>
     </div>
-)
-
+  );
 }
