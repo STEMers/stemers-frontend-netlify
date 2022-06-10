6dbb6eead7df392105c1eb8c0afdff18cc9b5d7a
AlinaTaoRao <raotao77@outlook.com>
2022-06-10 23:33:21 +0200

    add imgUrl in config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,14 @@
-export const localhostUrl="http://localhost:3000/api";
-export const herokuUrl="https://stemers-backend-heroku.herokuapp.com/api";
-export const baseUrl=herokuUrl;
+export const localhostUrl = "http://localhost:3000/api";
+export const herokuUrl = "https://stemers-backend-heroku.herokuapp.com/api";
+export const baseUrl = herokuUrl;
+
+export const localhostImgUrl = "http://localhost:1337";
+export const herokuImgUrl = "https://stemers-backend-heroku.herokuapp.com";
+export const imgUrl = localhostImgUrl;
+
+/* 
+ref:
+ "url": "/uploads/thumbnail_image_1_1_5a916903c0.png",
+ http://localhost:1337/uploads/image_1_1_5a916903c0.png
+
+*/
