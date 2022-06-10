144630e9b72e4f350979f5eb4cb9e30dcbe053cc
AlinaTaoRao <raotao77@outlook.com>
2022-06-11 00:03:41 +0200

    comment

--- src/config.js
+++ src/config.js
@@ -8,7 +8,9 @@ export const imgUrl = localhostImgUrl;
 
 /* 
 ref:
- "url": "/uploads/thumbnail_image_1_1_5a916903c0.png",
- http://localhost:1337/uploads/image_1_1_5a916903c0.png
+ "url": "/uploads/thumbnail_image_1_1_5a916903c0.png",  
+ "url": "/uploads/image_1_1_5a916903c0.png",
+ http://localhost:1337/uploads/image_1_1_5a916903c0.png  // localhost,works
+ https://stemers-backend-heroku.herokuapp.com/uploads/image_1_1_5a916903c0.png   // 404:NotFoundError
 
 */
