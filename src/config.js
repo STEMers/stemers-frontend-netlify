91c46f75f1cfae78d1875e1ae50bb9f14e2a9822
AlinaTaoRao <raotao77@outlook.com>
2022-06-11 08:05:34 +0200

    test heroku imgUrl, not work

--- src/config.js
+++ src/config.js
@@ -1,10 +1,12 @@
 export const localhostUrl = "http://localhost:3000/api";
 export const herokuUrl = "https://stemers-backend-heroku.herokuapp.com/api";
 export const baseUrl = herokuUrl;
+// export const baseUrl = localhostUrl;
 
 export const localhostImgUrl = "http://localhost:1337";
 export const herokuImgUrl = "https://stemers-backend-heroku.herokuapp.com";
 export const imgUrl = localhostImgUrl;
+// export const imgUrl = herokuImgUrl; // 404 not found
 
 /* 
 ref:
