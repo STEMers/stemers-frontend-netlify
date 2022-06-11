ac1f73f3b535b7fc162f6a79ca8ae1636944c164
Ashenafi <ashuewa@gmail.com>
2022-06-11 15:01:20 +0200

    assets for homepage addded

---
+++ src/json-data/home/Stars.js
@@ -0,0 +1,77 @@
+import shikka from "./shikka.png";
+import gelila from "./gelila.png";
+import raza from "./raza.png";
+import haifa from "./haifa.png";
+import sarah from "./sarah.png";
+import caroline from "./caroline.png";
+import cristi from "./cristi.png";
+import iva from "./iva.png";
+import badge1 from"./badge1.png";
+import badge2 from"./badge2.png";
+import badge3 from"./badge3.png";
+export const stars =[
+    {
+        name:"Shikka",
+        job:"CTO Google",
+        category:"Science",
+        country : "FR",
+        photo:  shikka,
+        badge:badge2
+    },
+    {
+        name:"Gelila",
+        job:"CTO-HYF",
+        category:"Engineering",
+        country : "ET",
+        photo:  gelila,
+        badge:badge1
+    },
+    {
+        name:"Raza",
+        job:"R&D Uber",
+        category:"Mathematics",
+        country : "JP",
+        photo:  raza,
+        badge:badge3
+    },
+    {
+        name:"Haifa",
+        job:"Student-ULB",
+        category:"Technology",
+        country : "LB",
+        photo:  haifa,
+        badge:badge2
+    },
+        {
+        name:"Sarah",
+        job:"Researcher-XYZ",
+        category:"Engineering",
+        country : "BR",
+        photo:  sarah,
+        badge:badge1
+    },
+        {
+        name:"Cristi",
+        job:"Manager Amazon",
+        category:"Mathematics",
+        country : "CR",
+        photo:  cristi,
+        badge:badge2
+    },
+        {
+        name:"Caroline",
+        job:"Lead Scientist",
+        category:"Technology",
+        country : "BE",
+        photo:  caroline,
+        badge:badge1
+    },
+    {
+        name:"Iva",
+        job:"Founder ABC",
+        category:"Engineering",
+        country : "DK",
+        photo:  iva,
+        badge:badge1
+    },
+];
