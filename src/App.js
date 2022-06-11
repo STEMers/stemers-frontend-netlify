1812e537aefedc72621f435ce5d73174d0c3432c
Ashenafi <ashuewa@gmail.com>
2022-06-11 14:04:06 +0200

    Unused Link removed from App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import { useState } from "react";
 
 import "./App.css";
