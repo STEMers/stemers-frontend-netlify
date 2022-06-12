d9801c63cd4ca69e8b52d69031e415fcdf4c643d
AlinaTaoRao <raotao77@outlook.com>
2022-06-12 19:32:18 +0200

    seperate component User form Stars, add styles

---
+++ src/components/stars/user/User.js
@@ -0,0 +1,41 @@
+import countryFlagEmoji from "country-flag-emoji";  // module issue
+
+import "./styles.css";
+
+export default function User({
+  srcAvatar,
+  userId,
+  firstName,
+  lastName,
+  country,
+  countrySN,
+  nominationsR,
+  badge1,
+  badge2,
+  badge3,
+}) {
+  return (
+    <div className="user card">
+      <img src={srcAvatar} alt="user face" className="user-avatar" />
+      <img
+        src={
+          nominationsR.length === 1
+            ? badge1
+            : nominationsR.length > 1 && nominationsR.length <= 3
+            ? badge2
+            : badge3
+        }
+        className="user-badge"
+        alt="badge"
+      />
+      <div className="user-short-info">
+        <p className="user-name" id={userId}>
+          {`${firstName} ${lastName}`}
+        </p>
+        <span className="user-country-sn">{countrySN}</span>
+        {/* <span className="user-flag">{countryFlagEmoji.get(country).emoji}</span> */}
+      </div>
+      
+    </div>
+  );
+}
