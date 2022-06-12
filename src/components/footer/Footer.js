f34baa580b98b263fabdbae7622ade549909dde9
Sanne Sofie <sannesofie@me.com>
2022-06-12 15:10:59 +0200

    adding social media icons with links to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,17 @@
-import './styles.css';
+import "./styles.css";
 import React from "react";
+import { FaFacebookSquare, FaPhoneAlt } from "react-icons/fa";
+import { BsInstagram } from "react-icons/bs";
 
 const Footer = () => {
-    return (
+return (
     <div className="footer">
-        <p>Template Footer</p>
+    
+    <div className="social-icons">
+    <a href="0471 65 34 02"><FaPhoneAlt style={{ color: "white", fontSize: "30px" }}  /></a> <a href="example.com"><FaFacebookSquare style={{ color: "white", fontSize: "30px" }} /></a> <a href="example.com"> <BsInstagram style={{ color: "white", fontSize: "30px" }} /></a>
     </div>
-    );   
+    </div>
+);
 };
 
 export default Footer;
