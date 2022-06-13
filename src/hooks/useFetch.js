0b8dc7f6a3c0348f10cb9f3a665fb872591778da
AlinaTaoRao <raotao77@outlook.com>
2022-06-13 09:54:32 +0200

    edit useFetch version 2, reusable

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 
-const useFetch = (url, selectedCategory, selectedCountry, newEvent) => {
+/* useFetch version 2: out put raw json data, reuseable, official way */
+const useFetch = (url, newEvent) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -12,30 +13,18 @@ const useFetch = (url, selectedCategory, selectedCountry, newEvent) => {
         const response = await fetch(encodeURI(url));
         if (!response.ok) {
           const errorJson = await response.json();
-          const message = `\nStatus: ${response.status} \nStatus Text: ${response.statusText} \nMessage:${errorJson.error.message} \nurl-> ${url}`;
+          const message = `\nStatus: ${response.status} \nStatus Text: ${response.statusText} \nMessage:${errorJson.error.message} \nurl: ${url}`;
           throw new Error(message);
         }
 
         const json = await response.json();
         console.log("json", json);
 
-        const filteredUsers = json
-          .filter((user) => user.category !== null)
-          .filter((user) => user.country !== null)
-          .filter((user) =>
-            !selectedCategory ? true : user.category.type.toLowerCase() === selectedCategory
-          )
-          .filter((user) =>
-            !selectedCountry ? true : user.country.name.toLowerCase() === selectedCountry
-          );
-        console.log("filteredUsers", filteredUsers);
-
-        setData(filteredUsers); // output filtered data 
+        setData(json); 
 
         setLoading(false);
       } catch (error) {
         alert(error);
-        //   setGlobalError(error);  //  ?
         setLoading(false);
       }
     };
@@ -45,5 +34,54 @@ const useFetch = (url, selectedCategory, selectedCountry, newEvent) => {
 
   return { data, loading };
 };
-
 export default useFetch;
+
+/* cancelled useFetch->useFetch version 1: out put filtered data, limited use case */
+
+// const useFetch = (url, selectedCategory, selectedCountry, newEvent) => {
+//   const [data, setData] = useState(null);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       setLoading(true);
+
+//       try {
+//         const response = await fetch(encodeURI(url));
+//         if (!response.ok) {
+//           const errorJson = await response.json();
+//           const message = `\nStatus: ${response.status} \nStatus Text: ${response.statusText} \nMessage:${errorJson.error.message} \nurl-> ${url}`;
+//           throw new Error(message);
+//         }
+
+//         const json = await response.json();
+//         console.log("json", json);
+
+//         const filteredUsers = json
+//           .filter((user) => user.category !== null)
+//           .filter((user) => user.country !== null)
+//           .filter((user) =>
+//             !selectedCategory ? true : user.category.type.toLowerCase() === selectedCategory
+//           )
+//           .filter((user) =>
+//             !selectedCountry ? true : user.country.name.toLowerCase() === selectedCountry
+//           );
+//         console.log("filteredUsers", filteredUsers);
+
+//         setData(filteredUsers); // output filtered data 
+
+//         setLoading(false);
+//       } catch (error) {
+//         alert(error);
+//         //   setGlobalError(error);  //  ?
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchData();
+//   }, [url, newEvent]);
+
+//   return { data, loading };
+// };
+
+// export default useFetch;
