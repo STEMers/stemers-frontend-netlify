cb2475c9645c2afd8a36f03786a42ac29ab2c22c
Sanne Sofie <sannesofie@me.com>
2022-06-16 15:00:23 +0200

    adding IconContext to footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,16 +2,26 @@ import "./styles.css";
 import React from "react";
 import { FaFacebookSquare, FaPhoneAlt } from "react-icons/fa";
 import { BsInstagram } from "react-icons/bs";
+import { IconContext } from "react-icons";
 
 const Footer = () => {
-return (
+  return (
     <div className="footer">
-    
-    <div className="social-icons">
-    <a href="0471 65 34 02"><FaPhoneAlt style={{ color: "white", fontSize: "30px" }}  /></a> <a href="example.com"><FaFacebookSquare style={{ color: "white", fontSize: "30px" }} /></a> <a href="example.com"> <BsInstagram style={{ color: "white", fontSize: "30px" }} /></a>
+      <div className="social-icons">
+        <IconContext.Provider value={{ color: "white", fontSize: "30px" }}>
+          <a href="1-800-12345">
+            <FaPhoneAlt />
+          </a>
+          <a href="www.faceboonk.ch">
+            <FaFacebookSquare />
+          </a>
+          <a href="www.inggagram.ch">
+            <BsInstagram />
+          </a>
+        </IconContext.Provider>
+      </div>
     </div>
-    </div>
-);
+  );
 };
 
 export default Footer;
