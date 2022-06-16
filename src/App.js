f89501e23e2a78116c9a1582140c817608a3e245
AlinaTaoRao <raotao77@outlook.com>
2022-06-16 09:53:34 +0200

    add currentUserData state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from "react";
 
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import Home from './components/home/Home';
-import About from './components/about/About';
-import Profile from './components/profile/Profile';
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Profile from "./components/profile/Profile";
 // import SignInOrSignUP from './components/sign-in/SignInOrSignUp';
-import Stars from './components/stars/Stars';
-import { Login, Register } from './routes';
+import Stars from "./components/stars/Stars";
+import { Login, Register } from "./routes";
 
 function App() {
+  // sign in or sign up state
+  const [currentUserData, setCurrentUserData] = useState(null); // store sign in/sign up user data
+  
   return (
     <div className="App">
       <Router>
@@ -19,8 +23,15 @@ function App() {
           <Route path="/stars" element={<Stars />} />
           <Route path="/about" element={<About />} />
           <Route path="/profile/:id" element={<Profile />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route
+            path="/login"
+            element={<Login />}
+            setCurrentUserData={setCurrentUserData}
+          />
+          <Route
+            path="/register"
+            element={<Register setCurrentUserData={setCurrentUserData} />}
+          />
         </Routes>
       </Router>
       <Footer />
